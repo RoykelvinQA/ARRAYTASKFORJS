d3cf616b4b0c0e035a7bfc3b7556095162e26dab
RoykelvinQA <adebiyitemitope88@gmail.com>
2024-12-15 20:55:35 -0800

    second push

--- Array.js
+++ Array.js
@@ -1,16 +1,34 @@
-const friend1 = "Bolaji"
-const friend2 = "Dele" 
-const friend3 = "Demola"
-const friend4 = "Femi"
-const friend5 = "Wasiu"
+// const friend1 = "Bolaji"
+// const friend2 = "Dele" 
+// const friend3 = "Demola"
+// const friend4 = "Femi"
+// const friend5 = "Wasiu"
 
-const friend = [
-    "Bolaji",
+// const friend = [
+//     "Bolaji",
+//     "Dele",
+//     "Demola",
+//     "Femi",
+//     "Wasiu"
+// ]
+
+// const bestFriend = friend[0]
+// console.log (bestFriend)
+
+let friend1 = "Dele" 
+let friend2 = "Femi" 
+let friend3 = "Wasiu" 
+let friend4 = "Demola" 
+let friend5 = "Bolaji" 
+
+
+let friends = [
     "Dele",
-    "Demola",
     "Femi",
-    "Wasiu"
+    "Wasiu",
+    "Demola",
+    "Bolaji"
 ]
-
-const bestFriend = friend[3]
-console.log (bestFriend)
+console.log(friends)
+let bestFriend = friends[4] + ", " + friends[3]
+console.log(bestFriend)
